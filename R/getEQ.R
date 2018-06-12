c81eb0a6e02a5b143a2d5a49364d4755a1d69e56
koopmans-michaela <koopmans.michaela@epa.gov>
2018-06-12 13:50:07 -0400

    Adjusted variable names

--- R/getEQ.R
+++ R/getEQ.R
@@ -2,20 +2,20 @@
 getEQ <- function(dataset, method){
   if (method =="SVR") {
       # support vector regression using e1071 library
-      SVRqsar <- e1071::svm(Y~., data = dataset, cost = 150, epsilon = 0.05, gamma = 0.00014)
-      return (SVMqsar)
+      SVReq <- e1071::svm(Y~., data = dataset, cost = 150, epsilon = 0.05, gamma = 0.00014)
+      return (SVMeq)
   } else if (method == "PLS") {
       # partial least squares using pls library
-      PLSqsar<-pls::plsr(Y~., data = dataset, ncomp = 3, scale=TRUE)
-      return(PLSqsar)
+      PLSeq<-pls::plsr(Y~., data = dataset, ncomp = 3, scale=TRUE)
+      return(PLSeq)
   } else if (method == "RF") {
       # random forest regression using randomforest library
-      RFqsar <- randomForest::randomForest(Y~., data = dataset, ntree = 500, nodesize = 5, mtry = 200, importance = TRUE, na.action = na.omit)
-      return(RFqsar)
+      RFeq <- randomForest::randomForest(Y~., data = dataset, ntree = 500, nodesize = 5, mtry = 200, importance = TRUE, na.action = na.omit)
+      return(RFeq)
   } else if (method == "MLR") {
       # multiple linear regression using base R linear model functions
-      MLRqsar <- lm(Y~., data = dataset)
-      return(MLSqsar)
+      MLReq <- lm(Y~., data = dataset)
+      return(MLSeq)
   } else {
       stop ("Specify valid method")
   }
