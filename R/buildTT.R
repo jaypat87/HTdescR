6378577a83efcba51ea8c158b25d9265af1715c2
koopmans-michaela <koopmans.michaela@epa.gov>
2018-06-13 10:31:08 -0400

    Made all function variables into global variables

--- R/buildTT.R
+++ R/buildTT.R
@@ -1,35 +1,35 @@
 
 buildTT <- function(TrainingSet, TestSet){
   #Use TrainingSet to get equations for all four methods
-  TrainEqSVR <- getEQ(TrainingSet, method = "SVR")
-  TrainEqPLS <- getEQ(TrainingSet, method = "PLS")
-  TrainEqRF <- getEQ(TrainingSet, method = "RF")
-  TrainEqMLR <- getEQ(TrainingSet, method = "MLR")
+  TrainEqSVR <<- getEQ(TrainingSet, method = "SVR")
+  TrainEqPLS <<- getEQ(TrainingSet, method = "PLS")
+  TrainEqRF <<- getEQ(TrainingSet, method = "RF")
+  TrainEqMLR <<- getEQ(TrainingSet, method = "MLR")
 
   #Predict TrainingSet Y for all methods
-  TrainYSVR <- predY(TrainEqSVR, TrainingSet)
-  TrainYPLS <- predY(TrainEqPLS, TrainingSet)
-  TrainYRF <- predY(TrainEqRF, TrainingSet)
-  TrainYMLR <- predY(TrainEqMLR, TrainingSet)
+  TrainYSVR <<- predY(TrainEqSVR, TrainingSet)
+  TrainYPLS <<- predY(TrainEqPLS, TrainingSet)
+  TrainYRF <<- predY(TrainEqRF, TrainingSet)
+  TrainYMLR <<- predY(TrainEqMLR, TrainingSet)
 
   #Get TrainingSet r2 for all methods
-  Trainr2SVR <- getr2(TrainYSVR, MTrainingSet)
-  Trainr2PLS <- getr2(TrainYPLS, MTrainingSet)
-  Trainr2RF <- getr2(TrainYRF, MTrainingSet)
-  Trainr2MLR <- getr2(TrainYMLR, MTrainingSet)
+  Trainr2SVR <<- getr2(TrainYSVR, MTrainingSet)
+  Trainr2PLS <<- getr2(TrainYPLS, MTrainingSet)
+  Trainr2RF <<- getr2(TrainYRF, MTrainingSet)
+  Trainr2MLR <<- getr2(TrainYMLR, MTrainingSet)
 
 
   #Predict TestSet Y for all methods
-  TestYSVR <- predY(TrainEqSVR, TestSet)
-  TestYPLS <- predY(TrainEqPLS, TestSet)
-  TestYRF <- predY(TrainEqRF, TestSet)
-  TestYMLR <- predY(TrainEqMLR, TestSet)
+  TestYSVR <<- predY(TrainEqSVR, TestSet)
+  TestYPLS <<- predY(TrainEqPLS, TestSet)
+  TestYRF <<- predY(TrainEqRF, TestSet)
+  TestYMLR <<- predY(TrainEqMLR, TestSet)
 
   #Get TestSet r2 for all methods
-  Testr2SVR <- getr2(TestYSVR, MTestSet)
-  Testr2PLS <- getr2(TestYPLS, MTestSet)
-  Testr2RF <- getr2(TestYRF, MTestSet)
-  Testr2MLR <- getr2(TestYMLR, MTestSet)
+  Testr2SVR <<- getr2(TestYSVR, MTestSet)
+  Testr2PLS <<- getr2(TestYPLS, MTestSet)
+  Testr2RF <<- getr2(TestYRF, MTestSet)
+  Testr2MLR <<- getr2(TestYMLR, MTestSet)
 
 
 
