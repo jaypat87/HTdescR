f18149d3c423c2f5b9f65305ef33c0a6296a49b5
koopmans-michaela <koopmans.michaela@epa.gov>
2018-06-15 09:16:15 -0400

    Changed training set equation calls to directly call ML methods so we can adjust defaults if needed

--- R/buildTT.R
+++ R/buildTT.R
@@ -1,10 +1,12 @@
 
-buildTT <- function(TrainingSet, TestSet){
+buildTT <- function(TrainingSet){
   #Use TrainingSet to get equations for all four methods
-  TrainEqSVR <<- getEQ(TrainingSet, method = "SVR")
-  TrainEqPLS <<- getEQ(TrainingSet, method = "PLS")
-  TrainEqRF <<- getEQ(TrainingSet, method = "RF")
-  TrainEqMLR <<- getEQ(TrainingSet, method = "MLR")
+  TrainEqSVR <<- e1071::svm(Y~., data = TrainingSet, cost = 150, epsilon = 0.05, gamma = 0.00014)
+  TrainEqPLS <<- pls::plsr(Y~., data = TrainingSet, ncomp = 3, scale=TRUE)
+  TrainEqRF <<- randomForest::randomForest(Y~., data = TrainingSet, ntree = 500, nodesize = 5, mtry = 200, importance = TRUE, na.action = na.omit)
+  TrainEqMLR <<- lm(Y~., data = TrainingSet)
+
+  #Add equations to package data
 
 }
 
