cc0ae298ab9e0f3fa80a88004ef4f9f0bcf13e19
michaela-koopmans <michaela.koopmans@gmail.com>
2018-06-18 11:31:00 -0400

    Clarified variable name for measured Ys

--- R/getr2.R
+++ R/getr2.R
@@ -1,6 +1,6 @@
 
-getr2 <- function(predictedY, mdataset) {
-  r2<-lm(predictedY[,,2] ~ mdataset = MeasuredYTest)
+getr2 <- function(predictedY, MeasuredY) {
+  r2<-lm(predictedY[,,2] ~ MeasuredY)
   summary(r2) #Is this needed? Should summary be returned instead?
   return(r2)
 }
