a5ee0d5e1da02c6416abc376f710d5ed91c15ec8
michaela-koopmans <michaela.koopmans@gmail.com>
2018-06-18 11:29:26 -0400

    Added comments about summary call

--- R/getr2.R
+++ R/getr2.R
@@ -1,6 +1,6 @@
 
 getr2 <- function(predictedY, mdataset) {
   r2<-lm(predictedY[,,2] ~ mdataset = MeasuredYTest)
-  summary(r2)
+  summary(r2) #Is this needed? Should summary be returned instead?
   return(r2)
 }
