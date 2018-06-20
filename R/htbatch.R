0e536161f064fc84e1a8473236a242b5b48bca44
John R. Olmstead <john.r.olmstead@gmail.com>
2018-06-20 13:48:37 -0400

    Added: Fill r1.hammett.value and r2.hammett value
    functionality: if statement that checks for smiles in meta ortho and para. If smiles are present meta1, meta2, ortho1, ortho2, para1 are summed for r1 and r2 respectively.
    
    if smiles are not present the else statement returns a 0 value for the hammett.value

--- R/htbatch.R
+++ R/htbatch.R
@@ -203,6 +203,20 @@ htbatch <- function (file, sigma.selection, ...) {
     } else {
         qsardataframe$r2.para1.value[i] = 0
     }
+
+    if (is.na(qsardataframe$r1.meta1.smiles[i]) | is.na(qsardataframe$r1.ortho1.smiles[i]) | is.na(qsardataframe$r1.para1.smiles[i]) == FALSE) {
+      qsardataframe$r1.hammett.value[i] <- sum(qsardataframe$r1.meta1.value[i], qsardataframe$r1.meta2.value[i], qsardataframe$r1.ortho1.value[i], qsardataframe$r1.ortho2.value[i], qsardataframe$r1.para1.value[i])
+
+    } else {
+        qsardataframe$r1.hammett.value[i] = 0
+    }
+
+    if (is.na(qsardataframe$r2.meta1.smiles[i]) | is.na(qsardataframe$r2.ortho1.smiles[i]) | is.na(qsardataframe$r2.para1.smiles[i]) == FALSE) {
+      qsardataframe$r2.hammett.value[i] <- sum(qsardataframe$r2.meta1.value[i], qsardataframe$r2.meta2.value[i], qsardataframe$r2.ortho1.value[i], qsardataframe$r2.ortho2.value[i], qsardataframe$r2.para1.value[i])
+
+    } else {
+        qsardataframe$r2.hammett.value[i] = 0
+    }
   }
 
   closeAllConnections()
