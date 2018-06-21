eb2792dee72758b682e2cb97003dbb4bef56b575
John R. Olmstead <john.r.olmstead@gmail.com>
2018-06-21 15:43:55 -0400

    Update: 2) add sigma meta, para, ortho in sigma hammett columns
    for loop added with iteration for the summation of meta, para, and ortho values.  If no smiles are found for meta, para, or ortho then 0 is the sum

--- R/cleanframe.R
+++ R/cleanframe.R
@@ -6,6 +6,15 @@
 library(tidyr)
 cleanmerge <- function (qsardataframe, ...) {
   qsardataframe <- replace_na(qsardataframe, replace = list(r1.meta1.value = 0, r1.meta2.value = 0, r1.ortho1.value = 0, r1.ortho2.value = 0, r1.para1.value = 0, r2.meta1.value = 0, r2.meta2.value = 0, r2.ortho1.value = 0, r2.ortho2.value = 0, r2.para1.value = 0))
+  i = 1
+  n <- nrow (qsardataframe)
 
+  for (i in 1:n) {
+    if (is.na(qsardataframe$r1.meta1.smiles[i]) | is.na(qsardataframe$r1.ortho1.smiles[i]) | is.na(qsardataframe$r1.para1.smiles[i]) == FALSE) {
+      qsardataframe$r1.hammett.value[i] <- sum(qsardataframe$r1.meta1.value[i], qsardataframe$r1.meta2.value[i], qsardataframe$r1.ortho1.value[i], qsardataframe$r1.ortho2.value[i], qsardataframe$r1.para1.value[i])
+
+    } else {
+      qsardataframe$r1.hammett.value[i] = 0
+    }
   return(qsardataframe)
 }
