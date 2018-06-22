2e6d5d67afadacbe1d08d388f201ab081e0d8a5e
Jay M. Patel <jaypatel87@gmail.com>
2018-06-23 04:46:58 +0530

    Added tidyr to description and fixed cleanframe.R

--- R/cleanframe.R
+++ R/cleanframe.R
@@ -3,9 +3,8 @@
 #' Cleaning the dataframe created by htbatch
 #'
 #'
-library(tidyr)
 cleanmerge <- function (qsardataframe, ...) {
-  qsardataframe <- replace_na(qsardataframe, replace = list(r1.meta1.value = 0, r1.meta2.value = 0, r1.ortho1.value = 0, r1.ortho2.value = 0, r1.para1.value = 0, r2.meta1.value = 0, r2.meta2.value = 0, r2.ortho1.value = 0, r2.ortho2.value = 0, r2.para1.value = 0))
+  qsardataframe <- tidyr::replace_na(qsardataframe, replace = list(r1.meta1.value = 0, r1.meta2.value = 0, r1.ortho1.value = 0, r1.ortho2.value = 0, r1.para1.value = 0, r2.meta1.value = 0, r2.meta2.value = 0, r2.ortho1.value = 0, r2.ortho2.value = 0, r2.para1.value = 0))
   i = 1
   n <- nrow (qsardataframe)
 
