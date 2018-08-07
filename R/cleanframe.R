01d7fb8858019c093de346c5bedea00023ecde0b
michaela-koopmans <michaela.koopmans@gmail.com>
2018-08-07 15:02:36 -0400

    Updated name of cleanframe function and adjusted output to leave out R1 Hammett Value

--- R/cleanframe.R
+++ R/cleanframe.R
@@ -3,7 +3,7 @@
 #' Cleaning the dataframe created by htbatch
 #'
 #'
-cleanmerge <- function (qsardataframe, ...) {
+cleanframe <- function (qsardataframe, ...) {
   qsardataframe <- tidyr::replace_na(qsardataframe, replace = list(r1.meta1.value = 0, r1.meta2.value = 0, r1.ortho1.value = 0, r1.ortho2.value = 0, r1.para1.value = 0, r2.meta1.value = 0, r2.meta2.value = 0, r2.ortho1.value = 0, r2.ortho2.value = 0, r2.para1.value = 0))
   i = 1
   n <- nrow (qsardataframe)
@@ -23,7 +23,7 @@ cleanmerge <- function (qsardataframe, ...) {
       qsardataframe$r2.hammett.value[i] = 0
     }
   }
-  qsardataframe <- cbind(qsardataframe[5],qsardataframe[55], qsardataframe[56], qsardataframe[63], qsardataframe[64], qsardataframe[65], qsardataframe[72])
+  qsardataframe <- cbind(qsardataframe[5],qsardataframe[55], qsardataframe[56], qsardataframe[64], qsardataframe[65], qsardataframe[72])
   as.data.frame(qsardataframe)
   return(qsardataframe)
 }
