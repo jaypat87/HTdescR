a2a4ea19bac582200c1ccab60a556ae3f4fb7a8c
John R. Olmstead <john.r.olmstead@gmail.com>
2018-08-16 15:44:26 -0400

    .R object file additions for documentation

---
+++ R/es.R
@@ -0,0 +1,14 @@
+#' Sigma values of steric fragments
+#'
+#' A dataset containing the fragment SMILES and various sigma values coming from datasets or
+#' calculated from the dataset amalgamation.
+#'
+#' @format A dataframe with 260 rows and 10 variables
+#'\describe{
+#' \item{sigma}{fragment SMILES, epi.value}
+#' }
+#' @source
+#'
+#'
+#'
+"es"
