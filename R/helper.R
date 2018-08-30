d4336496b42c495bd8b0625189e3ecb84f14ba05
John R. Olmstead <john.r.olmstead@gmail.com>
2018-08-30 12:38:40 -0400

    helper now has R skeleton and package defined for filter.

--- R/helper.R
+++ R/helper.R
@@ -1,8 +1,15 @@
-#Helper function for htdesc
-#Grabs values for phenyl fragments without running htdesc
-
+#' Expidited search for phenyl fragment sigma values
+#'
+#' This function is for returning values for the phenyl fragment without running htdesc.
+#' @usage helper(type, sigma.select)
+#'
+#' @param type Matches the HT.type specified in htbatch function value
+#' @param sigma.select The type of sigma to be returned; valid inputs include "A", "B", "C", "D", "E", "F", "G", "H", and "U"
+#'
+#' @return values from library that is inserted directly into qsardataframe.
+#'
 helper <- function(type, sigma.select){
-  hfilter <- filter(htdescHelper, HT.type == type, sigma.selection == sigma.select)
+  hfilter <- dplyr::filter(htdescHelper, HT.type == type, sigma.selection == Sigma.select)
   return(hfilter)
-}
+  }
 
